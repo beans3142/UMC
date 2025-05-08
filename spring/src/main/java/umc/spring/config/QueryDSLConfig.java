dca46cfecbeb06674f78ce7dc4524f99a716644c
beans3142 <greenbeans3142@naver.com>
2025-05-09 01:14:11 +0900

    week 6

--- spring/src/main/java/umc/spring/config/QueryDSLConfig.java
+++ spring/src/main/java/umc/spring/config/QueryDSLConfig.java
@@ -1,19 +1,19 @@
 package umc.spring.config;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
 
 @Configuration
 @RequiredArgsConstructor
 public class QueryDSLConfig {
-
     private final EntityManager entityManager;
 
     @Bean
-    public JPAQueryFactory jpaQueryFactory() {
+    public JPAQueryFactory jpaQueryFactory(){
         return new JPAQueryFactory(entityManager);
     }
 }
---
+++ spring/src/main/java/umc/spring/service/StoreService/StoreQueryService.java
@@ -0,0 +1,12 @@
+package umc.spring.service.StoreService;
+
+import java.util.List;
+import java.util.Optional;
+
+import umc.spring.domain.Store;
+
+public interface StoreQueryService {
+
+    Optional<Store> findStore(Long id);
+    List<Store> findStoresByNameAndScore(String name, Float score);
+}
