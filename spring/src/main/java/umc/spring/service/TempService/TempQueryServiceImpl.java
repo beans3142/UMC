a3d212b5ca1a38c211a9d9fbc2c89051f4b92fdf
beans3142 <greenbeans3142@naver.com>
2025-05-09 03:22:17 +0900

    week7

---
+++ spring/src/main/java/umc/spring/service/TempService/TempQueryServiceImpl.java
@@ -0,0 +1,19 @@
+package umc.spring.service.TempService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import umc.spring.apiPayload.code.status.ErrorStatus;
+import umc.spring.apiPayload.code.exception.handler.TempHandler;
+
+@Primary
+@Service
+@RequiredArgsConstructor
+public class TempQueryServiceImpl implements TempQueryService{
+
+    @Override
+    public void CheckFlag(Integer flag) {
+        if (flag == 1)
+            throw new TempHandler(ErrorStatus.TEMP_EXCEPTION);
+    }
+}
